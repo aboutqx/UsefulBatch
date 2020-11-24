8aa40bbd1d982d641cff8e10a3397384b6402891
aboutqx <1004945442@qq.com>
2020-11-24 15:12:33 +0800

    Update BuildUE.bat

--- BuildUE.bat
+++ BuildUE.bat
@@ -1,8 +1,8 @@
 @REM "F:\Program\Epic Games\UE_4.25\Engine\Binaries\Win64\UE4Editor-Cmd.exe" F:\NSLY\NSLY.uproject -run=Cook  -TargetPlatform=WindowsNoEditor -fileopenlog -unversioned  -stdout -CrashForUAT -unattended -NoLogTime
  "G:\Program\Epic Games\UE_4.25\Engine\Build\BatchFiles\RunUAT.bat" BuildCookRun -project=G:\NSLY\NSLY.uproject -clientconfig=Shipping -platform=Win64 -cook -stage -pak -stagingdirectory=G:\UEPackage\NSLY\ -package -build 
  
-&&"C:\Program Files\7-Zip\7z.exe" a WindowsNoEditor.7z G:\UEPackage\NSLY\WindowsNoEditor\
+&&Xcopy /E /I G:\NSLY\UI G:\UEPackage\NSLY\WindowsNoEditor\NSLY\UI
 
-Xcopy /E /I G:\NSLY\UI G:\UEPackage\NSLY\WindowsNoEditor\NSLY\UI
+"C:\Program Files\7-Zip\7z.exe" a WindowsNoEditor.7z G:\UEPackage\NSLY\WindowsNoEditor\
 
 copy .\WindowsNoEditor.7z  \\Tyto001\qxsy /Z /Y
